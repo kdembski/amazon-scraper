df993b7eb8ca29adb64f18bfd802703e5280a867
kdembski <krldembski@gmail.com>
2025-01-24 08:50:55 +0100

    fix: fashion category scraping only brand name

--- src/builders/AmazonPlpAdBuilder.ts
+++ src/builders/AmazonPlpAdBuilder.ts
@@ -6,7 +6,7 @@ export class AmazonPlpAdBuilder {
   build(item: Element, category: string) {
     const asin = item.getAttribute("data-asin");
     const image = item.querySelector("img")?.getAttribute("src");
-    const name = item.querySelector("h2 > span")?.textContent;
+    const name = item.querySelector("a > h2 > span")?.textContent;
 
     if (!asin || !image || !name) return this;
 
