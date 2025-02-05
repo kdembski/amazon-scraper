63be191eee4f1c8e00e2c6c00c666a83966f7fc3
kdembski <krldembski@gmail.com>
2025-02-05 10:05:20 +0100

    Removed headers causing captcha

--- src/services/AmazonService.ts
+++ src/services/AmazonService.ts
@@ -55,12 +55,6 @@ export class AmazonService {
             "User-Agent": userAgent,
             Referer: referer,
             "Referrer-Policy": "strict-origin-when-cross-origin",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
-            Accept:
-              "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            DNT: "1",
-            Connection: "keep-alive",
           },
         })
         .then((response) => {
