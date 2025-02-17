ab38df7105f53464d7a74b0074ef034e0322f227
kdembski <krldembski@gmail.com>
2025-02-17 14:24:45 +0100

    Added request speed counter

--- src/services/RequestQueueService.ts
+++ src/services/RequestQueueService.ts
@@ -2,6 +2,10 @@ import { ArgsService } from "@/services/ArgsService";
 
 export class RequestQueueService {
   private argsService;
+  private lastRequestCount = 0;
+  private requestCount = 0;
+  private speedHistory: number[] = [];
+  private speed = 0;
   failed = 0;
   pending = 0;
   pendingLimit;
@@ -16,10 +20,16 @@ export class RequestQueueService {
     this.argsService = argsService;
 
     setInterval(() => {
+      this.requestCount = this.pending + this.queue.length;
+      this.updateSpeedHistory();
+      this.calculateSpeed();
+
       console.log(
         logWrapper,
-        `pending: ${this.pending} | queue: ${this.queue.length} | failed: ${this.failed}`
+        `speed: ${this.speed}/s | pending: ${this.pending} | queue: ${this.queue.length} | failed: ${this.failed}`
       );
+
+      this.lastRequestCount = this.requestCount;
     }, 1000);
   }
 
@@ -58,5 +68,26 @@ export class RequestQueueService {
     if (this.pending >= this.pendingLimit) return;
     const next = this.queue.shift();
     next?.();
+  }
+
+  private updateSpeedHistory() {
+    if (!this.lastRequestCount || !this.requestCount) return;
+
+    const speed = this.lastRequestCount - this.requestCount;
+    if (speed < 0) return;
+
+    const length = this.speedHistory.unshift(speed);
+    this.speedHistory.length = Math.min(length, 100);
+  }
+
+  private calculateSpeed() {
+    if (!this.speedHistory.length) {
+      return;
+    }
+
+    this.speed = Math.round(
+      this.speedHistory.reduce((sum, v) => sum + v, 0) /
+        this.speedHistory.length
+    );
   }
 }
